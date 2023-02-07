95eca7a59c6360e86731453f932d306c635ff216
Peter Yoo <trustintofu@gmail.com>
2023-02-06 16:15:52 -0800

    feat(next.config.js): Adds cache header for font files.

--- next.config.js
+++ next.config.js
@@ -12,6 +12,37 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/fonts/GTWalsheimPro-Regular.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/GTWalsheimPro-Medium.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/GTWalsheimPro-Bold.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
