0997a41f45009e5cf9c7da79f03689dbaf00041b
Peter Yoo <trustintofu@gmail.com>
2023-02-06 21:28:37 -0800

    style(_app): Adds layout for entire app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-screen w-screen bg-[#E5E5E5] flex justify-center items-center">
+      <Component {...pageProps} />
+    </div>
+  );
 }
