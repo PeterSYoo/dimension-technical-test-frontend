7e8145cc70717b73a70950ea5c67fa561ed99d15
Peter Yoo <trustintofu@gmail.com>
2023-02-06 21:29:21 -0800

    feat(_document): Adds preloaded custom fonts.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        rel="preload"
+        href="/fonts/GTWalsheimPro-Regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href="/fonts/GTWalsheimPro-Medium.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href="/fonts/GTWalsheimPro-Bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
