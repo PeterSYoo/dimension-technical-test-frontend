d1b49854041f099d5511193c301be30e0ed39202
Peter Yoo <trustintofu@gmail.com>
2023-02-06 16:30:43 -0800

    feat(tailwind.config.js): Adds GTWalsheimPro font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,7 +11,16 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'GTWalsheimPro-Regular',
+          'GTWalsheimPro-Medium',
+          'GTWalsheimPro-Bold',
+          ...fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [],
 };
